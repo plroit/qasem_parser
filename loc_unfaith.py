d35d72a0c0a831e6e46dfe308c1af91ab3279a2b
Arie <arie.cattan@gmail.com>
2023-12-22 12:37:22 +0200

    cleanAnswerId + sourceIds + token-level labels

--- loc_unfaith.py
+++ loc_unfaith.py
@@ -9,17 +9,36 @@ from spacy.tokens import Doc
 import jsonlines
 from qasem_parser import QasemParser, QasemFrame, QasemArgument
 import argparse 
+from difflib import SequenceMatcher
+
+IMPORTANT_POS = ['ADJ', 'ADV', 'NOUN', 'NUM', 'PROPN', 'VERB']
+STR_RESEMBLANCE_THR = 0.86
 
 def flatten_list(lst):
   return list(chain.from_iterable(lst))
-
 
 class LocUnfaith:
   def __init__(self, summary_id, summary_sentences) -> None:
     self.summary_id = summary_id
     self.df_sentences = summary_sentences
-    
+  
+  def is_aligned(self, t1, t2):
+    return SequenceMatcher(None, t1.lower(), t2.lower()).ratio() > STR_RESEMBLANCE_THR
 
+  def _get_source_alignment(self, source_tokens, span):
+    aligned_tokens = set()
+    
+    all_source_aligned_tokens = set([i for i, src_tok in enumerate(source_tokens) 
+                                 if any([self.is_aligned(src_tok.lemma_, sum_tok) for sum_tok in span])])
+    for src_tok in all_source_aligned_tokens:
+      # either the source token is an important POS or there is an aligned token in a window of 2 with an important token
+      if source_tokens[src_tok].pos_ in IMPORTANT_POS or \
+          (src_tok+1 in all_source_aligned_tokens and source_tokens[src_tok+1].pos_ in IMPORTANT_POS) or \
+          (src_tok-1 in all_source_aligned_tokens and source_tokens[src_tok-1].pos_ in IMPORTANT_POS):
+        aligned_tokens.add(src_tok)
+
+    return list(aligned_tokens)
+    
   def _extract_qas_from_sentence(self, sentence_id, row):
     qas = []
     for predicate_frame in row["qa_frames"]:
@@ -27,9 +46,24 @@ class LocUnfaith:
       predicate_token_index = predicate_frame.predicate.index + row["start_sentence_token"] 
 
       for frame in predicate_frame.arguments:
+        # remove non-informative POS in the answer 
+        # this enables to merge nodes in the graph (e.g., in Paris vs. Paris)
+        # and show only the clean span in the first annotation step
+        clean_start_answer = frame.start_token 
+        while clean_start_answer <= frame.end_token and row["sentences"][clean_start_answer].pos_ not in IMPORTANT_POS:
+          clean_start_answer += 1
+        if clean_start_answer > frame.end_token:
+          print(f"The answer {frame.text} is not a valid answer")
+          continue
+        
         # update start and end tokens of answers at the summary level
+        clean_start_answer += row["start_sentence_token"]
         answer_start_token_index = frame.start_token + row["start_sentence_token"] 
-        answer_end_token_index = frame.end_token + row["start_sentence_token"] 
+        answer_end_token_index = frame.end_token + row["start_sentence_token"]
+
+        # get source alignment to the answer
+        lemma_span = [x.lemma_ for x in row["sentences"][frame.start_token:frame.end_token]]
+        answer_source_alignment = self._get_source_alignment(row["spacy_source"], lemma_span)
 
         qas.append({
           "sentId": sentence_id,
@@ -41,7 +75,9 @@ class LocUnfaith:
           "answerStartToken": [answer_start_token_index],
           "answerEndToken": [answer_end_token_index],
           "answerId": f'{answer_start_token_index}-{answer_end_token_index}',
-          "verbTokenId": frame.verb_token_id
+          "cleanAnswerId": f'{clean_start_answer}-{answer_end_token_index}',
+          "verbTokenId": frame.verb_token_id,
+          "sourceIds": answer_source_alignment
         })
 
     return qas     
@@ -74,7 +110,7 @@ class LocUnfaith:
       predicate_tokens.add(int(predicate_id.split("-")[0]))
       predicates[predicate_id] = predicate_qas["questionId"].tolist()
       for i, qa in predicate_qas.iterrows():
-        answer_id = qa["answerId"]
+        answer_id = qa["cleanAnswerId"] # use cleanAnswerId to merge different answers to the same span
         answers[answer_id].append(qa["questionId"])
 
     spans = []
@@ -106,8 +142,12 @@ class LocUnfaith:
       })
 
     spans = sorted(spans, key=lambda x: x["start"])
-    for i, _ in enumerate(spans):
+    for i, span in enumerate(spans):
       spans[i]["id"] = i
+      start, end = span["start"], span["end"]
+      span_text = [x.lemma_ for x in self.df_sentences.iloc[0]["spacy_summary"][start:end]]
+      source_alignment = self._get_source_alignment(self.df_sentences.iloc[0]["spacy_source"], span_text)
+      spans[i]["sourceIds"] = source_alignment
       
     return spans 
   
@@ -132,13 +172,18 @@ class LocUnfaith:
       for token_id in range(span["start"], span["end"]):
         token2span[token_id].append(i)
 
+    labels = [None] * len(self.df_sentences.iloc[0]["spacy_summary"])
+    if "labels" in self.df_sentences.columns: # in CLIFF
+      labels = self.df_sentences.iloc[0]["labels"]
+
     for i, token in enumerate(self.df_sentences.iloc[0]["spacy_summary"]):
       tokens.append({
         "id": i,
         "text": token.text,
         "lemma": token.lemma_,
         "spans": token2span[i],
-        "class": "token" if len(token2span[i]) == 0 else "mention"
+        "class": "token" if len(token2span[i]) == 0 else "mention",
+        "label": labels[i]
       })
     
     return tokens
